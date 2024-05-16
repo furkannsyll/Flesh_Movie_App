b446ea7c872ccdb1b292301be852d14405b07e95
Furkan Soylu <fsoylu567@gmail.com>
2024-05-17 00:29:00 +0300

    Tmdb Api Setup-Axios, Components, Pages, Photos

---
+++ fleshmovie/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import fetchDataFromApi from './utils/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getApiConfiguration } from './store/homeSlice';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+
+
+function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi('/movie/popular')
+      .then((res) => {
+        console.log(res);
+        dispatch(getApiConfiguration(res));
+      })
+      .catch((error) => {
+        console.error('Error fetching API data:', error);
+      });
+  };
+
+  return (
+    <div className="App">{url?.total_pages}</div>
+  );
+}
+
+export default App;
